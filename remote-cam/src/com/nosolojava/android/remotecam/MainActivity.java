a799bd6187342d174c119867446cdc6dadb9e895
Carlos Verdes <cverdes@gmail.com>
2015-02-17 00:00:07 +0100

    Remote cam first commit

---
+++ remote-cam/src/com/nosolojava/android/remotecam/MainActivity.java
@@ -0,0 +1,33 @@
+package com.nosolojava.android.remotecam;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import com.nosolojava.android.fsm.view.impl.BasicFSMActivity;
+import com.nosolojava.android.remotecam.service.MainFSMService;
+
+public class MainActivity extends BasicFSMActivity {
+	private static final String DISCONNECTED_STATE = "disconnected-state";
+
+	private static final Uri FSM_URI = Uri
+			.parse("android.resource://com.nosolojava.fsm.android.firstSteps/raw/fsm#remotecamSession");
+
+	public MainActivity() {
+
+		super(MainFSMService.class, R.layout.loading_layout, FSM_URI);
+	}
+
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		this.associateStateView(R.layout.disconnected_layout, null, DISCONNECTED_STATE);
+	}
+
+
+
+
+}
---
+++ remote-cam/src/com/nosolojava/android/remotecam/service/MainFSMService.java
@@ -0,0 +1,24 @@
+package com.nosolojava.android.remotecam.service;
+
+import android.content.Intent;
+
+import com.nosolojava.android.fsm.service.FSMServiceImpl;
+
+public class MainFSMService extends FSMServiceImpl {
+
+	@Override
+	public void onStart(Intent intent, int startId) {
+		// TODO Auto-generated method stub
+		super.onStart(intent, startId);
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	
+
+
+}
